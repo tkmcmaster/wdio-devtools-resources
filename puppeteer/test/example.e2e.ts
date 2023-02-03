2d91290f64d4bf719b4840d2b399e7abfdb07edc
Trevor McMaster <trevor.mcmaster@familysearch.org>
2023-02-03 08:04:19 -0700

    Basic initial test, no resources yet

---
+++ puppeteer/test/example.e2e.ts
@@ -0,0 +1,17 @@
+describe('Basic Test', () => {
+    const timeout: number = 60000;
+    let resourceCount = 0;
+
+    it('should go to page and log resources', async () => {
+        await browser.maximizeWindow();
+        // Set up resource counting
+
+
+        await browser.url('/africa/');
+        browser.waitUntil(() => document.readyState === "complete", { timeout });
+        await $('//*[contains(text(),"Create Your Family Tree")]').waitForExist({ timeout });
+
+        await expect($('h4')).toBeExisting();
+    });
+});
+
