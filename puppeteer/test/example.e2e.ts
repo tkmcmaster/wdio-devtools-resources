92ce2ce21e136d266be5fc16f3fee83f755e6ff3
Trevor McMaster <trevor.mcmaster@familysearch.org>
2023-02-03 09:23:25 -0700

    Added counting of resource requests

--- puppeteer/test/example.e2e.ts
+++ puppeteer/test/example.e2e.ts
@@ -1,17 +1,35 @@
+import CDP from "devtools-protocol";
+import { CDPSession } from "puppeteer-core";
+
 describe('Basic Test', () => {
-    const timeout: number = 30000;
-    let resourceCount = 0;
+  const timeout: number = 30000;
+  let resourceCount = 0;
 
-    it('should go to page and log resources', async () => {
-        await browser.maximizeWindow();
-        // Set up resource counting
+  it('should go to page and log resources', async () => {
+    await browser.maximizeWindow();
 
-
-        await browser.url('/africa/');
-        browser.waitUntil(() => document.readyState === "complete", { timeout });
-        await $('//*[contains(text(),"Create Your Family Tree")]').waitForExist({ timeout });
-
-        await expect($('h4')).toBeExisting();
+    // Set up resource counting
+    const puppeteer = await browser.getPuppeteer();
+    const [page] = await puppeteer.pages();
+    if (!page) {
+      throw new Error("Could not load the puppeeter page");
+    }
+    // const cdpSession: CDPSession = await puppeteer.target().createCDPSession() as unknown as CDPSession;
+    const cdpSession: CDPSession = await page.target().createCDPSession() as unknown as CDPSession;
+    console.log("cdpSession init", { sessionId: browser.sessionId, cdpSessionId: cdpSession.id });
+    await cdpSession.send("Network.enable", { }),
+    cdpSession.on("Network.requestWillBeSent", (e: CDP.Network.RequestWillBeSentEvent) => {
+      resourceCount++;
+      // console.log("requestWillBeSent", { resourceCount, e });
     });
+
+    await browser.url('/africa/');
+    browser.waitUntil(() => document.readyState === "complete", { timeout });
+    await $('//*[contains(text(),"Create Your Family Tree")]').waitForExist({ timeout });
+
+    await expect($('h4')).toBeExisting();
+    console.log("requestWillBeSent resourceCount: " + resourceCount);
+    expect(resourceCount).toBeGreaterThanOrEqual(100);
+  });
 });
 
